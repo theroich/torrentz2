37bafc5c9bb2dd26a78711c1dd229b67c641c501
Roi Castro Casal <rcastro@emetel.local>
2017-05-19 10:18:53 +0200

    fist commit

---
+++ README.md
@@ -0,0 +1,35 @@
+## TORRENTZ2 API
+### Install
+Download zip and decompress in node_modules folder
+```
+cd torrentz2
+npm install
+```
+
+### Search magnet links 
+```javascript
+const torrentz = require('torrentz2');
+torrentz.searchTorrentz2('Ubuntu 15.04').then(console.log);
+/*
+{ magnet: 'magnet:?xt=urn:btih:7eb63698c76f30c03a74462af56163963c07b902
+            &dn=ubuntu-pack-14.04.2-mate&tr=udp%3A%2F%2Ftracker.coppersurfer.tk%3A696
+            9%2Fannounce&tr=udp%3A%2F%2Ftracker.leechers-paradise.org%3A6969%2Fannoun
+            ce&tr=udp%3A%2F%2Ftracker.zer0day.to%3A1337%2Fannounce&tr=udp%3A%2F%2Fexp
+            lodie.org%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker1.wasabii.com.tw%3A6969
+            %2Fannounce&tr=http%3A%2F%2F173.254.204.71%3A1096%2Fannounce',
+    name: 'ubuntu-pack-14.04.2-mate',
+    size: '2729 MB',
+    peers: '1',
+    seeds: '2' },
+  { magnet: 'magnet:?xt=urn:btih:18ac50d74c61883b3ab4c40f5dd3e35f157de1a2
+            &dn=ubuntu-14.04-desktop-amd64.iso&tr=udp%3A%2F%2Ftracker.coppersurfer.tk
+            %3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.leechers-paradise.org%3A6969%2F
+            announce&tr=udp%3A%2F%2Ftracker.zer0day.to%3A1337%2Fannounce&tr=udp%3A%2F
+            %2Fexplodie.org%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker1.wasabii.com.tw%
+            3A6969%2Fannounce&tr=http%3A%2F%2F173.254.204.71%3A1096%2Fannounce',
+                name: 'ubuntu-14.04-desktop-amd64.iso',
+    size: '964 MB',
+    peers: '1',
+    seeds: '2' },...
+*/
+```
---
+++ app.js
@@ -0,0 +1,57 @@
+/**
+ * Created by theroich on 18/5/17.
+ */
+
+
+const request = require('request');
+const cheerio = require('cheerio');
+const _ = require('lodash');
+
+const Q  = require('q');
+
+exports.searchTorrentz2 = function(searchStr){
+    const deferred = Q.defer();
+    const option = {url: `https://torrentz2.eu/search?f=${searchStr}`};
+    request(option, function (err, resp, html) {
+
+
+        var $ = cheerio.load(html);
+        const values = _($('dl')).filter(tag => $($(tag).find('a[href]')).attr('href') && $($(tag).find('a[href]')).attr('href').indexOf('?') == -1)
+            .map(extractHtmlData).sortBy(magnetObj => parseInt(magnetObj.seeds)).reverse()
+            .value();
+
+
+        deferred.resolve(values);
+
+        function extractHtmlData(obj){
+            const hash = $($(obj).find('a[href]')).attr('href').substring(1);
+
+            const name =  $($(obj).find('a[href]')).text();
+            const size = $($(obj).find('dd span')[2]).text();
+            const peers = $($(obj).find('dd span')[3]).text();
+            const seeds = $($(obj).find('dd span')[4]).text();
+            const magnet = `magnet:?xt=urn:btih:${hash}&dn=${encodeURI(name)}${getTrackerStr()}`;
+            return {magnet,name,size,peers,seeds};
+
+        }
+
+    });
+    return deferred.promise;
+};
+
+
+
+function getTrackerStr(){
+
+    const trackers = [
+        'udp://tracker.coppersurfer.tk:6969/announce',
+        'udp://tracker.leechers-paradise.org:6969/announce',
+        'udp://tracker.zer0day.to:1337/announce',
+        'udp://explodie.org:6969/announce',
+        'udp://tracker1.wasabii.com.tw:6969/announce',
+        'http://173.254.204.71:1096/announce'
+    ];
+    return '&tr='+_.join(_(trackers).map(encodeURIComponent).value(),'&tr=');
+}
+
+

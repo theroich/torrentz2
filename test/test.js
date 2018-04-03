5006bded6068eb6df4c7e46d2245db8021c9d3d2
Bastien Arata <banst@Bastiens-MacBook-Pro.local>
2018-04-03 15:42:29 +0200

    :zap: improve performances

--- test/test.js
+++ test/test.js
@@ -1,6 +1,34 @@
 /**
  * Created by theroich on 7/8/17.
  */
-var torrentz = require('../app.js');
+var torrentz = require('../app');
+var torremtzOld = require('./app.old');
+var Benchmark = require('benchmark');
+var suite = new Benchmark.Suite();
+var fs = require('fs');
 
-torrentz.searchTorrentz2('Game of thrones s07e04').then(console.log);
+let values = torrentz.searchTorrentz2('Harry Potter').then(values => {
+  console.log('New', values.length);
+});
+values = torremtzOld.searchTorrentz2('Harry Potter').then(values => {
+  console.log('Old', values.length);
+});
+
+const html = fs.readFileSync('./test/resources/raw.html');
+
+suite
+  .add('new', function() {
+    const values = torrentz.parseHtmlResponse(html);
+  })
+  .add('old', function() {
+    const values = torremtzOld.parseHtmlResponse(html);
+  })
+  // add listeners
+  .on('cycle', function(event) {
+    console.log(String(event.target));
+  })
+  .on('complete', function() {
+    console.log('Fastest is ' + this.filter('fastest').map('name'));
+  })
+  // run async
+  .run({ async: true });
